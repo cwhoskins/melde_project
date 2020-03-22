0a5e482b44be038730fcc2d0cc494320622d0c56
cwhoskins <cwhoskins@live.com>
2020-03-21 21:34:20 -0700

    formula_directivity
    Script now calculates the directivity from the HPBW formula from the Book (Table 6.2)

--- Project_1_script.m
+++ Project_1_script.m
@@ -7,8 +7,8 @@ theta = theta_deg .* (pi/180);
 %Wavelength
 lambda = 0.03;
 k = 2*pi / lambda;
-d = lambda / 4;
-num_elements = 2;
+d = lambda / 2;
+num_elements = 8;
 amp = ones(1, num_elements);
 phase = 0:(num_elements-1);
 phase = phase .* (pi/num_elements); 
@@ -20,7 +20,8 @@ u_array = u .* af;
 u_array_norm = abs(u_array ./ max(u_array));
 u_array_db = 10 .* log10(u_array_norm);
 plot(theta_deg, u_array_norm);
-%plot(theta_deg, u_array_db);
+%Estimate Directivity based on book formula
+directivity_formula = 2 *((pi/2) - acos(1.391 * lambda / (pi * num_elements * d)))
 %Compute Directivity
 directivity = find_directivity(u_array_norm, theta)
 %Compute Beamwidth
